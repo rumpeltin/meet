caeb3010fe4df79eb3e70429e862f460e6d5c4f5
rumpeltin <76797616+rumpeltin@users.noreply.github.com>
2023-03-22 21:50:53 +0100

    added correct endpoints

--- src/api.js
+++ src/api.js
@@ -40,7 +40,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'YOUR_GET_EVENTS_API_ENDPOINT' + '/' + token;
+    const url = "https://48pwfyunxc.execute-api.eu-central-1.amazonaws.com/dev/api/token" + "/" + token;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
@@ -62,7 +62,7 @@ export const getAccessToken = async () => {
     const code = await searchParams.get("code");
     if (!code) {
       const results = await axios.get(
-        "YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT"
+        "https://48pwfyunxc.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
       );
       const { authUrl } = results.data;
       return (window.location.href = authUrl);
