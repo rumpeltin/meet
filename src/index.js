de6ec5690522aa6b5582b09dc421387b4b7ab99d
rumpeltin <76797616+rumpeltin@users.noreply.github.com>
2023-04-11 16:52:47 +0200

    implemented atatus for monitoring

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +22,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.config('6b0768b199684152abaa51399fc44b89').install();
