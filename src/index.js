482c1f9dca9c6e5b13811b021b1528c04d47ce06
rumpeltin <76797616+rumpeltin@users.noreply.github.com>
2023-04-19 18:23:49 +0200

    design fixes app.js

--- src/index.js
+++ src/index.js
@@ -20,4 +20,8 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
 
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
 
